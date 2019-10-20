cba8623c9824ea41175abd2541ab40ba6a9ad926
Anurag Rajshekhar <anurag.r0601@gmail.com>
2019-10-20 15:00:57 +0530

    FaceDetection model works. Logging in smile prob values.

--- app/src/main/java/com/song4me/PickMyMoodActivity.java
+++ app/src/main/java/com/song4me/PickMyMoodActivity.java
@@ -1,12 +1,25 @@
 package com.song4me;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.camerakit.CameraKitView;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.common.FirebaseVisionImageMetadata;
+import com.google.firebase.ml.vision.face.FirebaseVisionFace;
+import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetector;
+import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetectorOptions;
+
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -19,6 +32,7 @@ public class PickMyMoodActivity extends AppCompatActivity {
     FloatingActionButton fabCapture;
     @BindView(R.id.cameraKitView)
     CameraKitView cameraKitView;
+    private String TAG = "PickMyMood";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +60,38 @@ public class PickMyMoodActivity extends AppCompatActivity {
                 
                 else {
                     Toast.makeText(PickMyMoodActivity.this, "Success", Toast.LENGTH_SHORT).show();
+
+                    Bitmap imageBitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                    FirebaseVisionImageMetadata metadata = new FirebaseVisionImageMetadata.Builder()
+                            .setWidth(1000)   // 480x360 is typically sufficient for
+                            .setHeight(1400)  // image recognition
+                            .setFormat(FirebaseVisionImageMetadata.IMAGE_FORMAT_NV21)
+                            .build();
+
+                    FirebaseVisionFaceDetectorOptions options =
+                            new FirebaseVisionFaceDetectorOptions.Builder()
+                                    .setClassificationMode(FirebaseVisionFaceDetectorOptions.ALL_CLASSIFICATIONS)
+                                    .enableTracking()
+                                    .build();
+
+                    FirebaseVisionFaceDetector detector = FirebaseVision.getInstance().getVisionFaceDetector(options);
+
+                    FirebaseVisionImage firebaseVisionImage = FirebaseVisionImage.fromBitmap(imageBitmap);
+
+                    Task<List<FirebaseVisionFace>> result = detector.detectInImage(firebaseVisionImage)
+                            .addOnSuccessListener(new OnSuccessListener<List<FirebaseVisionFace>>() {
+                                @Override
+                                public void onSuccess(List<FirebaseVisionFace> firebaseVisionFaces) {
+                                    for (FirebaseVisionFace face : firebaseVisionFaces) {
+                                        Log.d(TAG, "**********");
+                                        Log.d(TAG, "face ["+face+"]");
+                                        Log.d(TAG, "Smiling Prob ["+face.getSmilingProbability()+"]");
+                                        Log.d(TAG, "Left eye open ["+face.getLeftEyeOpenProbability()+"]");
+                                        Log.d(TAG, "Right eye open ["+face.getRightEyeOpenProbability()+"]");
+
+                                    }
+                                }
+                            });
                 }
             }
         });
